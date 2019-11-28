dd4e9a71a8cd0c37171a02d748b549d641a0971e
Pello Altadill <pello.altadill@gluckgames.com>
2019-11-28 10:27:04 +0100

    Added more fields to table

--- todo/models/todo.py
+++ todo/models/todo.py
@@ -4,16 +4,16 @@ from django.utils.translation import gettext_lazy as _
 class Todo(models.Model):
     """A model for todo"""
     task = models.CharField(_("task"), max_length=400, help_text=_("Enter task info"))
-    active = models.BooleanField(default=True)
-    status = models.CharField(choices=(
+    active = models.BooleanField(_("active"),default=True)
+    status = models.CharField(_("status"),choices=(
             (0, "To do"),
             (1, "Doing"),
             (2, "Done"),
         ),
         max_length=1
     )
-    created_at = models.DateTimeField(auto_now=True)
-    modified_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(_("created_at"), auto_now=True)
+    modified_at = models.DateTimeField(_("modified_at"),auto_now_add=True)
     task_type = models.ForeignKey("TaskType",on_delete=models.CASCADE, default=1)
 
     # Other options: help_text, verbose_name, default, null(True), blank(True), primary_key (true)
