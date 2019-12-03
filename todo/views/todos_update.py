e929c24679e9ec09b94558f5afe54bf4a6bc7046
Pello Altadill <pello.altadill@gluckgames.com>
2019-12-03 11:25:04 +0100

    Working update

--- todo/views/todos_update.py
+++ todo/views/todos_update.py
@@ -9,25 +9,8 @@ class TodosUpdate(View):
     initial = {'task': 'Write your task'}
     template_name = 'todos.html'
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, id, *args, **kwargs):
         form = self.form_class(initial=self.initial)
-        todos = Todo.objects.all()
-        print(todos)
-        return render(request, "base.html", {'template_name': self.template_name, 'form': form, 'todos': todos})
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            # <process form cleaned data>
-            todo = Todo(task=form.cleaned_data['task'])
-            todo.save()
-            print("Form is valid!!")
-            return HttpResponseRedirect('/todos')
-
-        return render(request, "base.html", {'template_name': self.template_name, 'form': form})
-
-    def update(self, request, id, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
-        todo = Todo.objects.get(id)
+        todo = 0 # Todo.objects.get(id)
         print("Im NOT IN UPDATE??")
         return render(request, "base.html", {'template_name': 'update.html', 'form': form, 'todo': todo})
