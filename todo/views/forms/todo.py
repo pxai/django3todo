6776cfdd7ab621aa51eccbca1a16a348c117c073
Pello Altadill <pello.altadill@gluckgames.com>
2019-12-08 18:20:33 +0100

    Update by hand

--- todo/views/forms/todo.py
+++ todo/views/forms/todo.py
@@ -4,8 +4,10 @@ from .validators.validate_text import validate_text
 from ...models.todo import Todo
 
 class TodoForm(ModelForm):
+    id = forms.IntegerField(widget=forms.HiddenInput(), required=False)
     task = forms.CharField(initial='class',label='Task', max_length=100, min_length=3, validators=[validate_text])
 
     class Meta:
         model = Todo
-        fields = ['task']
+        fields = ['id','task']
+        widgets = {'id': forms.HiddenInput()}
--- todo/views/todos_update.py
+++ todo/views/todos_update.py
@@ -11,7 +11,18 @@ class TodosUpdate(View):
 
     def get(self, request, id, *args, **kwargs):
         todo = Todo.objects.get(pk=id)
-        #form = self.form_class(initial={'task': todo.task}) # by hand
-        form  = TodoForm(request.POST, instance=todo)
+        form = self.form_class(initial={'task': todo.task, 'id': todo.id}) # by hand
+        # form  = TodoForm(request.POST, instance=todo)
         print("Updating this id: %s" % (id))
+        return render(request, "base.html", {'template_name': 'update.html', 'form': form, 'todo': todo})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            # <process form cleaned data>
+            todo = Todo(id=form.cleaned_data['id'],task=form.cleaned_data['task'])
+            todo.save()
+            print("Form is valid!!")
+            return HttpResponseRedirect('/todos')
+
         return render(request, "base.html", {'template_name': 'update.html', 'form': form})
